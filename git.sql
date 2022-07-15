4a256687a7b01b17baafbde25cf7018203147de1
Ranestari Sastriani <ranestarisastriani@Gmail.com>
2022-07-15 20:31:57 +0700

    checkout

--- git.sql
+++ git.sql
@@ -1,1 +1,7 @@
 test 1
+
+modifikasi -> git add -> git commit
+
+git log -> untuk melihat history git
+git rm {nama file }-> mengembalikan ke commit yang sebelumnya
+git  checkout {versi}  {nama file }-->  untuk melihat value di versi tersebut
