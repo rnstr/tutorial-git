a342b105f70883be046690c6b0c4277195fa3ab9
Ranestari Sastriani <ranestarisastriani@Gmail.com>
2022-07-15 21:08:58 +0700

    push

--- git.sql
+++ git.sql
@@ -6,3 +6,4 @@ git log -> untuk melihat history git
 git rm {nama file }-> mengembalikan ke commit yang sebelumnya
 git  checkout {versi}  {nama file }-->  untuk melihat value di versi tersebut
 git ignore {nama file} -> untuk meng ignore agar tidak perlu di track
+git push -u origin master
