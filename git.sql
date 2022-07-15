bacc62f8e0d473d7376e2a22bd6b1673f7d48803
Ranestari Sastriani <ranestarisastriani@Gmail.com>
2022-07-15 21:07:54 +0700

    add local

--- git.sql
+++ git.sql
@@ -5,3 +5,4 @@ modifikasi -> git add -> git commit
 git log -> untuk melihat history git
 git rm {nama file }-> mengembalikan ke commit yang sebelumnya
 git  checkout {versi}  {nama file }-->  untuk melihat value di versi tersebut
+git ignore {nama file} -> untuk meng ignore agar tidak perlu di track
